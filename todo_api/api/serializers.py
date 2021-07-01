82b0b935f21485ba7ecc2a85b84385c0e551c556
KD3821 <korden3821@gmail.com>
2021-07-02 00:12:01 +0300

    API views modified

--- todo_api/api/serializers.py
+++ todo_api/api/serializers.py
@@ -1,19 +1,36 @@
-from rest_framework.serializers import (Serializer, CharField, IntegerField)
+from rest_framework.serializers import (ModelSerializer, Serializer, CharField, IntegerField, HyperlinkedIdentityField,
+                                        SerializerMethodField)
 from notes.models import Note
 
 
-class NoteSerializer(Serializer):
-    id = IntegerField(read_only=True)
-    title = CharField(required=True, max_length=250)
-    text = CharField(required=False, allow_blank=True)
+class NoteSerializer(ModelSerializer):
+    author = SerializerMethodField(read_only=True)
+
+    def get_author(self, obj):
+        return str(obj.author.email)
+
+    class Meta:
+        model = Note
+        fields = '__all__'
 
 
-    def create(self, validated_data):
-        return Note.objects.create(**validated_data)
+class ThinNoteSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(view_name='notes-detail')
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'url')
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.text = validated_data.get('text', instance.text)
-        instance.save()
-        return instance
-    
+# class NoteSerializer(Serializer):
+#     id = IntegerField(read_only=True)
+#     title = CharField(required=True, max_length=250)
+#     text = CharField(required=False, allow_blank=True)
+#
+#
+#     def create(self, validated_data):
+#         return Note.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.text = validated_data.get('text', instance.text)
+#         instance.save()
+#         return instance
