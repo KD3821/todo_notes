603d0c9cf2bedcd31fc30e0e5628d722a0292874
KD3821 <korden3821@gmail.com>
2021-07-08 22:03:57 +0300

    topsecretkey is set

--- scraping_service/scraping_service/settings.py
+++ scraping_service/scraping_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6l5tcoy&d$&r=#2-ghzdc4zds6s9!n9r$m&29*t^sglegj=09r'
+SECRET_KEY = 'your own secret key 8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
