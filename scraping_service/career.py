acf75f5c07949c791ce18adf1927adb339e3a02d
KD3821 <korden3821@gmail.com>
2021-07-12 23:53:25 +0300

    overcome antiparser feachers

---
+++ scraping_service/career.py
@@ -0,0 +1,44 @@
+import requests
+import codecs
+from bs4 import BeautifulSoup as BS
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
+           'Accept':'text/html,application/xhtml+xml,applicatio/xml;q=0.9,*/*;q=0.8'
+           }
+
+
+jobs = []
+errors = []
+domain = 'https://spb.careerist.ru'
+url = 'https://spb.careerist.ru/jobs-python/'
+resp = requests.get(url, headers=headers)
+if resp.status_code == 200:
+    soup = BS(resp.content, 'html.parser')
+    main_div = soup.find('div', attrs={'class': 'vacSearchList'})
+    if main_div:
+        div_lst = main_div.find_all('div', attrs={'class': 'list  send-res-from-catalog-container '})
+        for div in div_lst:
+            title = div.find('p', attrs={'class': 'h5 card-text'})
+            href = title.a['href']
+            title_name = title.a.text
+            company = div.find('div', attrs={'class': 'm-b-10'})
+            company_name = company.a.text
+            description = div.find('p', attrs={'class': 'card-text'})
+            content = description.text
+            jobs.append({'title': title_name,
+                         'url': domain+href,
+                         'description': content,
+                         'company': company_name})
+    else:
+        errors.append({'url': url, 'title': "Div does not exist"})
+else:
+    errors.append({'url': url, 'title': "Page does not response"})
+
+
+
+h = codecs.open('career.txt', 'w', 'utf-8')
+for i in range(len(jobs)):
+    h.write(str(jobs[i]))
+    h.write(str('\n'))
+    h.close()
