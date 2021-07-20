23e083c6b33d6e8a388b87f84f4e937d29c9e271
KD3821 <korden3821@gmail.com>
2021-07-20 23:58:47 +0300

    send_email added

---
+++ scraping_service/send_emails.py
@@ -0,0 +1,46 @@
+import os, sys
+import django
+from django.core.mail import EmailMultiAlternatives
+from django.contrib.auth import get_user_model
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+django.setup()
+from scraping.models import Vacancy
+
+User = get_user_model()
+qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
+users_dict = {}
+for i in qs:
+    users_dict.setdefault((i['city'], i['language']), [])
+    users_dict[(i['city'], i['language'])].append(i['email'])
+
+if users_dict:
+    params = {'city_id__in':[], 'language_id__in':[]}
+    for pair in users_dict.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])
+    qs = Vacancy.objects.filter(**params).values()
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dict.items():
+        rows = vacancies.get(keys,[])
+        html = ''
+        for row in rows:
+            html += f'<h5><a href="{ row.url }">{ row.title }</a></h5>'
+
+
+
+
+
+
+subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
+text_content = 'This is an important message.'
+html_content = '<p>This is an <strong>important</strong> message.</p>'
+msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+msg.attach_alternative(html_content, "text/html")
+msg.send()
