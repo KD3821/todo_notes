aff4c2372e19c5a990b116667ed6e39e530f7dab
KD3821 <korden3821@gmail.com>
2021-07-21 17:19:43 +0300

    send_email editted

--- scraping_service/send_emails.py
+++ scraping_service/send_emails.py
@@ -1,5 +1,6 @@
 import os, sys
 import django
+import datetime
 from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth import get_user_model
 
@@ -8,7 +9,15 @@ sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 django.setup()
-from scraping.models import Vacancy
+from scraping.models import Vacancy, Error, Url
+from scraping_service.settings import EMAIL_HOST_USER
+ADMIN_USER = EMAIL_HOST_USER
+
+today = datetime.date.today()
+subject = f"Рассылка вакансий за {today}"
+text_content = f"Добрый день! Свежие вакансии по вашим предпочтениям за {today}."
+from_email = EMAIL_HOST_USER
+empty = '<h2>К сожалению, по Вашим предпочтениям данных нет.</h2>'
 
 User = get_user_model()
 qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
@@ -22,25 +31,50 @@ if users_dict:
     for pair in users_dict.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params).values()
+    qs = Vacancy.objects.filter(**params, timestamp=today).values()
     vacancies = {}
-    for i in qs:
-        vacancies.setdefault((i['city_id'], i['language_id']), [])
-        vacancies[(i['city_id'], i['language_id'])].append(i)
-    for keys, emails in users_dict.items():
-        rows = vacancies.get(keys,[])
-        html = ''
-        for row in rows:
-            html += f'<h5><a href="{ row.url }">{ row.title }</a></h5>'
+    # for i in qs:
+    #     vacancies.setdefault((i['city_id'], i['language_id']), [])
+    #     vacancies[(i['city_id'], i['language_id'])].append(i)
+    # for keys, emails in users_dict.items():
+    #     rows = vacancies.get(keys, [])
+    #     html = ''
+    #     for row in rows:
+    #         html += f'<h3><a href="{ row["url"] }">{ row["title"] }</a></h3>'
+    #         html += f'<p>{row["description"]}</p>'
+    #         html += f'<p>{row["company"]}</p><br><hr>'
+    #     _html = html if html else empty
+    #     for email in emails:
+    #         to = email
+    #         msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    #         msg.attach_alternative(_html, "text/html")
+    #         msg.send()
 
+qs = Error.objects.filter(timestamp=today)
+subject = ''
+text_content = ''
+to = ADMIN_USER
+_html = ''
 
+if qs.exists():
+    error = qs.first()
+    data = error.data
+    for i in data:
+        _html += f'<p><a href="{i["url"]}">Error: {i["title"]}</a></p><br>'
+    subject = f"Ошибки скрапинга {today}"
+    text_content = f"Ошибки скрапинга {today}"
 
+qs = Url.objects.all().values('city', 'language')
+urls_dict = {(i['city'], i['language']): True for i in qs}
+urls_err = ''
+for keys in users_dict.keys():
+    if keys not in urls_dict:
+        urls_err += f'<p>Для города { keys[0] } и специализации { keys[1] } отсутствуют urls</p><br>'
+if urls_err:
+    subject += 'Отсутствуют urls'
+    _html += urls_err
 
-
-
-subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
-text_content = 'This is an important message.'
-html_content = '<p>This is an <strong>important</strong> message.</p>'
-msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-msg.attach_alternative(html_content, "text/html")
-msg.send()
+if subject:
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(_html, "text/html")
+    msg.send()
