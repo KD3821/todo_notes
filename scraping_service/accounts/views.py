b3593dcfa28c03999027a9acfe8a1fbf4fac175b
KD3821 <korden3821@gmail.com>
2021-07-22 23:09:29 +0300

    contact_view added

--- scraping_service/accounts/views.py
+++ scraping_service/accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout, get_user_model
-from .forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+from .forms import UserLoginForm, UserRegistrationForm, UserUpdateForm, ContactForm
+from django.contrib import messages
+from scraping.models import Error
+import datetime as dt
 
 
 User = get_user_model()
@@ -28,25 +31,30 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Пользователь добавлен в систему.')
         return render(request, 'accounts/register_done.html', {'new_user': new_user})
     return render(request, 'accounts/register.html', {'form': form})
 
 
 def update_view(request):
+    contact_form = ContactForm()
     if request.user.is_authenticated:
         user = request.user
         if request.method == 'POST':
             form = UserUpdateForm(request.POST)
             if form.is_valid():
-                user.city = form.cleaned_data['city']
-                user.language = form.cleaned_data['language']
-                user.send_email = form.cleaned_data['send_email']
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
                 user.save()
+                messages.success(request, 'Данные сохранены.')
+                # return redirect('accounts:update')
                 return render(request, 'accounts/update_done.html', {'user': user})
-            return render(request, 'accounts/update.html', {'form': form})
-        else:
-            form = UserUpdateForm(initial={'city': user.city, 'language': user.language, 'send_email': user.send_email})
-            return render(request, 'accounts/update_done.html', {'user': user})
+            return render(request, 'accounts/update.html', {'form': form, 'contact_form': contact_form})
+
+        form = UserUpdateForm(initial={'city': user.city, 'language': user.language, 'send_email': user.send_email})
+        return render(request, 'accounts/update_done.html', {'user': user})
     else:
         return redirect('accounts:login')
 
@@ -57,7 +65,38 @@ def delete_view(request):
         if request.method == 'POST':
             qs = User.objects.get(pk=user.pk)
             qs.delete()
+            messages.error(request, 'Пользователь удален из системы.')
         return redirect('home')
+
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST or None)
+        if contact_form.is_valid():
+            data = contact_form.cleaned_data
+            city = data['city']
+            language = data['language']
+            email = data['email']
+            qs = Error.objects.filter(timestamp=dt.date.today())
+            if qs.exists():
+                err = qs.first()
+                data = err.data.get('user_data', [])
+                data.append({'city': city, 'email': email, 'language': language})
+                err.data['user_data'] = data
+                err.save()
+            else:
+                data = [{'city': city, 'email': email, 'language': language}]
+                Error(data=f"user_data:{data}").save()
+            messages.success(request, 'Данные сохранены для администрации.')
+            return redirect('accounts:update')
+        else:
+            return redirect('accounts:update')
+    else:
+        return redirect('accounts:login')
+
+
+
 
 
 
