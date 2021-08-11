8045e4f8c6fd59985383860f50da36703eee7b1e
KD3821 <korden3821@gmail.com>
2021-08-11 23:57:11 +0300

    secretly commited

--- scraping_service/scraping_service/settings.py
+++ scraping_service/scraping_service/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e3%&**dd2^wa08tfz31_)q4j=l&@4is5-h)ziw$8504%yh6q-j'
+SECRET_KEY = 'very-very secret key is used'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
